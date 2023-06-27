e5ec5b1c6ccf4535bea940f5a30ddd9436920c42
SanketLakhani <sanketlakhani80@gmail.com>
2023-06-27 18:29:45 +0530

    we have disabled the router and remove the about section in website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -33,29 +33,26 @@ function App() {
   };
 
   return (
-    <Router>
-      <div>
-        {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About />}></Route>
-            <Route
-              exact
-              path="/"
-              element={
-                <TextForm
-                  showAlert={showAlert}
-                  heading="Enter the text to analyze below"
-                  mode={mode}
-                />
-              }
-            ></Route>
-          </Routes>
-        </div>
+    // <Router>
+    <div>
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes> */}
+        {/* <Route exact path="/about" element={<About />}></Route> */}
+        {/* <Route exact path="/" element= */}
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to analyze below"
+          mode={mode}
+        />
+        {/* } */}
+        {/* ></Route> */}
+        {/* </Routes> */}
       </div>
-    </Router>
+    </div>
+    // {/* </Router> */}
   );
 }
 
